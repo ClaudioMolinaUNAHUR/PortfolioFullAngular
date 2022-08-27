8c3d964836813061f01a8400a119043068a30ffd
Claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2022-08-26 23:22:12 -0300

    se consume datos para about desde firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,31 @@ import { infoPagina } from '../interfaces/info-paginas.interface';
 export class InfoPaginaService {
   info: infoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-paginas.json')
       .subscribe( (resp:infoPagina) => {
           this.cargada = true;
           this.info = resp;
-          console.log(this.info);
+          // console.log(this.info);
       })
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-ed787-default-rtdb.firebaseio.com/equipo'+'.json')
+      .subscribe( resp =>{
+        let personas = Object.values(resp)
+        for(let persona of personas){
+          this.equipo.push(persona);
+        }
+        console.log(this.equipo);
+      })
+
   }
 }
